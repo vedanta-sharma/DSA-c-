8685230ecd5f8f6cd4bb13e55dce7ea8cedc51e4
Vedanta Sharma <vedanta.sharmaa@gmail.com>
2023-04-09 11:29:24 +0530

    added q10

--- Q10_numeric_palindrome_full_pyramid.cpp
+++ Q10_numeric_palindrome_full_pyramid.cpp
@@ -5,16 +5,28 @@ int main(){
     int n;
     cout<<"Enter the size: ";
     cin >> n;
+    int k = n;
     for(int row = 0; row<n; row = row + 1){
-        int k = n;
+        int c =1;
         for(int col = 0; col< k; col = col+1){
-            if(col==0 || col == row || row == n-1){
-                cout<<col+1;
-            }
-            else{
+            if(col<n-row-1){
                 cout<<" ";
             }
+            else if(col<n){
+                cout<<c;
+                c++;
+            }
+            else if(col == n){
+                c=c-2;
+                cout<<c;
+                c--;
+            }
+            else{
+                cout<<c;
+                c--;
+            }
         }
+        k++;
         cout<< endl;
     }
 }
