7896a2d030f8208854e8ace93eb8bed7540f6b3d
Vedanta Sharma <vedanta.sharmaa@gmail.com>
2023-04-13 00:16:45 +0530

    added q13

--- Q13_Fancy_pattern_2.cpp
+++ Q13_Fancy_pattern_2.cpp
@@ -8,16 +8,25 @@ int main(){
     int c=1;
     for(int row = 0; row<n; row = row + 1){
         for(int col = 0; col<=row; col = col+1){
-                cout<<c;
-                c++;
-                if(col<1){
-                    cout<<"*";
-                }
+            cout<<c;
+            c++;
+            if(col<row){
+                cout<<"*";
             }
-            cout<<endl;
         }
-        int start=c-n;
-        for(int i=0; )
+        cout<<endl;
+    }
+    int start=c-n;
+    for(int row=0; row<n; row=row+1){
+        int k= start;
+        for(int col=0; col<=n-row-1;col=col+1){
+            cout<<k;
+            k++;
+            if(col<n-row-1){
+                cout<<"*";
+            }
+        }
+        start=start-(n-row-1);
         cout<< endl;
     }
 }
