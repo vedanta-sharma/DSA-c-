9836489006d8eac821db3d13418072cb6202e138
Vedanta Sharma <vedanta.sharmaa@gmail.com>
2023-04-13 23:54:55 +0530

    added q13

--- Q11_solid_half_diamond.cpp
+++ Q11_solid_half_diamond.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     int n;
+//     cout<<"Enter the size: ";
+//     cin >> n;
+//     for(int row = 0; row<n; row = row + 1){
+//         for(int col = 0; col< row+1; col = col+1){
+//             cout<<"*";
+//         }
+//         cout<< endl;
+//     }
+//     for(int row = 0; row<n-1; row = row + 1){
+//         for(int col = 0; col< n- row-1; col = col+1){
+//             cout<<"*";
+//         }
+//         cout<< endl;
+//     }
+// }
 int main(){
     int n;
     cout<<"Enter the size: ";
