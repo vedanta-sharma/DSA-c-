0b9ab2eca4b45b453362d36a7060b34d15acd3c6
Vedanta Sharma <vedanta.sharmaa@gmail.com>
2023-04-18 22:26:38 +0530

    Modification

--- Arrays/Printing_vallues.cpp
+++ Arrays/Printing_vallues.cpp
@@ -17,5 +17,22 @@ int main(){
     for(int index=0; index< 10; index++){
         cout<<arr[index]<<" ";
     }
+    cout<<endl;
+
+    //Printing doubles
+
+    cout << "Doubles are: "<<endl;
+    for(int index=0; index< 10; index++){
+        cout<<2*arr[index]<<" ";
+    }
+    cout<<endl;
+
+    //Changing elements of array to 1
+
+    cout << "Changed array: "<<endl;
+    for(int index=0; index< 10; index++){
+        arr[index]=1;
+        cout<<arr[index]<<" ";
+    }
     return 0;
 }
