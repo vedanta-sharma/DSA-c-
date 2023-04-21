2d1ad690285ab0d3f4467ac24c9ead4ad18d4e85
Vedanta Sharma <vedanta.sharmaa@gmail.com>
2023-04-21 23:25:16 +0530

    Sort 0's and 1's

---
+++ Arrays/Sort_0s_1s.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> arr{0,1,0,1,1,0,0,0,1,1,1,0};
+    int start = 0;
+    int end = arr.size() - 1;
+    int i =0;
+
+    while( i != end ){
+        if(arr[i]==0){
+            swap(arr[start],arr[i]);
+            start++;
+            i++;
+        }
+        else{
+            swap(arr[end],arr[i]);
+            end--; 
+        }
+    }
+    for(int k=0; k<arr.size(); k++){
+        cout<<arr[k]<<" ";
+    }
+}
