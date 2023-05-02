0727aae8bce22405671c6649da604dc596617543
Vedanta Sharma <vedanta.sharmaa@gmail.com>
2023-05-02 21:46:51 +0530

    Modified

--- Arrays/Transpose.cpp
+++ Arrays/Transpose.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 //Creating function to print Transpose of the array
-void TransposeArr(int arr[][3], int rows, int cols){
+void TransposeArr(int arr[][3], int rows, int cols, int transpose[][3]){
     for(int i=0; i < rows;i++){
         for(int j=0; j < cols; j++){
-            swap(arr[i][j], arr[j][i]);
+            transpose[j][i]= arr[i][j];
         }
+    }
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            cout << arr[i][j]<<" ";
+        }
+        cout << endl;
     }
 }
 
@@ -15,23 +21,15 @@ int main(){
     int arr[3][3];
     int rows =3;
     int cols =3;
+    int transpose[3][3];
 
     //Taking Input for the array
     cout<<"Enter the elements: "<<endl;
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
-            cin>>arr[i][j];
+            cin>>arr[j][i];
         }
     }
-
-    //Printing the array
-    cout<<"Printing row-wise : "<<endl;
-    for(int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<"The Transpose of the array is : "<< T(arr, rows, cols);
-    return 0;
+    cout<<"The Transpose of the array is : "<<endl;
+    TransposeArr(arr, rows, cols, transpose);
 }
